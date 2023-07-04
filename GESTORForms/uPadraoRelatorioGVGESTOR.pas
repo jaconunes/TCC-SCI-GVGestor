306536d5b8296ed835c7ae34111e8ffb729c2382
jaconunes <jacksonnunes.bnu@gmail.com>
2023-07-04 17:54:23 -0300

    Atualizando projeto

--- GESTORForms/uPadraoRelatorioGVGESTOR.pas
+++ GESTORForms/uPadraoRelatorioGVGESTOR.pas
@@ -26,6 +26,7 @@ type
     SaveDialog1: TSaveDialog;
     procedure FormCreate(Sender: TObject);
     procedure btEditarReportClick(Sender: TObject);
+    procedure btSalvarComoClick(Sender: TObject);
   private
     { Private declarations }
     procedure fCarregaFrxPadrao(Tabela: TfrPadraoRelatorioGVGESTOR);
@@ -52,10 +53,44 @@ begin
   wReport.DesignReport;
 end;
 
+procedure TfrPadraoRelatorioGVGESTOR.btSalvarComoClick(Sender: TObject);
+var
+  wSaveDialog : TSaveDialog;
+  wPDFFile : TfrxPDFExport;
+  wDOCFile : TfrxDOCXExport;
+  wReport: TfrxReport;
+begin
+  wSaveDialog.FileName := Caption;
+  if SaveDialog1.Execute then
+    begin
+      if MessageDlg('Deseja abrir o arquivo exportado para visualização?', mtInformation, [mbYes,mbNo], 0) = mrYes then
+      begin
+        wPDFFile.OpenAfterExport := True;
+        wDOCFile.OpenAfterExport := True;
+      end
+      else
+      begin
+        wPDFFile.OpenAfterExport := False;
+        wDOCFile.OpenAfterExport := False;
+      end;
+      wPDFFile.FileName := StringReplace(SaveDialog1.FileName, ExtractFileExt(SaveDialog1.FileName), EmptyStr, [rfIgnoreCase]) + '.pdf';
+      wDOCFile.FileName := StringReplace(SaveDialog1.FileName, ExtractFileExt(SaveDialog1.FileName), EmptyStr, [rfIgnoreCase]) + '.docx';
+
+      wReport.PrepareReport;
+
+      case SaveDialog1.FilterIndex of
+      1: wReport.Export(wPDFFile);
+      2: wReport.Export(wDOCFile);
+      end;
+    end;
+end;
+
 procedure TfrPadraoRelatorioGVGESTOR.fCarregaFrxPadrao(
   Tabela: TfrPadraoRelatorioGVGESTOR);
+var
+  wReport :  TfrxReport;
 begin
-  TfrxReport.LoadFromFile('C:\TCC - Gestor de Vistorias\Reports\' + Tabela.Name + '.fr3');
+  wReport.LoadFromFile('C:\TCC - Gestor de Vistorias\Reports\' + Tabela.Name + '.fr3');
 end;
 
 procedure TfrPadraoRelatorioGVGESTOR.FormCreate(Sender: TObject);
