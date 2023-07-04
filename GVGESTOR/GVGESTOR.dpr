7af6af9b1b14292e73dd1b8b06ad4c6b216ecb60
jaconunes <jacksonnunes.bnu@gmail.com>
2023-07-04 17:27:49 -0300

    Atualizando projeto

--- GVGESTOR/GVGESTOR.dpr
+++ GVGESTOR/GVGESTOR.dpr
@@ -6,7 +6,8 @@ uses
   udmConnectionGVGestor in '..\GESTORForms\DataModule\udmConnectionGVGestor.pas' {dmConnection: TDataModule},
   udmDadosGVGESTOR in '..\GESTORForms\DataModule\udmDadosGVGESTOR.pas' {dmTabelas: TDataModule},
   uPadraoCadastroGVGESTOR in '..\GESTORForms\uPadraoCadastroGVGESTOR.pas' {frPadraoCadastroGVGESTOR},
-  uPadraoConsultaGVGESTOR in '..\GESTORForms\uPadraoConsultaGVGESTOR.pas' {frPadraoConsultaGVGSTOR};
+  uPadraoConsultaGVGESTOR in '..\GESTORForms\uPadraoConsultaGVGESTOR.pas' {frPadraoConsultaGVGSTOR},
+  uPadraoRelatorioGVGESTOR in '..\GESTORForms\uPadraoRelatorioGVGESTOR.pas' {frPadraoRelatorioGVGESTOR};
 
 {$R *.res}
 
@@ -18,5 +19,6 @@ begin
   Application.CreateForm(TfrLogin, frLogin);
   Application.CreateForm(TfrPadraoCadastroGVGESTOR, frPadraoCadastroGVGESTOR);
   Application.CreateForm(TfrPadraoConsultaGVGSTOR, frPadraoConsultaGVGSTOR);
+  Application.CreateForm(TfrPadraoRelatorioGVGESTOR, frPadraoRelatorioGVGESTOR);
   Application.Run;
 end.
